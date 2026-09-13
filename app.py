c5542fa5c86334b63b955d5eb81b5a449f2bb525
Azan Khan <azankokarai@gmail.com>
2026-09-13 14:37:58 +0500

    Register native download location routes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from core_app import app
 import media_features  # registers TikTok/Pinterest media routes
+import location_features  # registers native download-location routes
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
