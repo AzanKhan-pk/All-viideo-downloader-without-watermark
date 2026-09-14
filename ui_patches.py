b3f91b9280f60e7d2ec0a6cc259f073ae486c9a7
Azan Khan <azankokarai@gmail.com>
2026-09-14 13:52:50 +0500

    Load folder action position patch

--- ui_patches.py
+++ ui_patches.py
@@ -13,6 +13,7 @@ def inject_persistent_download_ui(response):
                 '<script src="/static/special_preview_ui.js"></script>',
                 '<script src="/static/text_selection_patch.js"></script>',
                 '<script src="/static/progress_size_stability_patch.js"></script>',
+                '<script src="/static/folder_action_position_patch.js"></script>',
             )
             if "</body>" in body:
                 for marker in markers:
