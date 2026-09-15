17a304f9614a53c80a70c4761e979d19f7f2ed2f
Azan Khan <azankokarai@gmail.com>
2026-09-15 21:18:18 +0500

    Inject in-app keyboard shortcuts overlay

--- ui_patches.py
+++ ui_patches.py
@@ -13,7 +13,8 @@ def inject_persistent_download_ui(response):
                 '<script src="/static/progress_size_stability_patch.js"></script>',
                 '<script src="/static/folder_action_position_patch.js"></script>',
                 '<script src="/static/final_ui_reliability_patch.js"></script>',
-                '<script src="/static/windows_keyboard_shortcuts.js"></script>'
+                '<script src="/static/windows_keyboard_shortcuts.js"></script>',
+                '<script src="/static/app_keyboard_shortcuts.js"></script>'
             )
             if "</body>" in body:
                 for marker in markers:
