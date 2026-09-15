b0554fba5df96237b981427a172f5f240400b784
Azan Khan <azankokarai@gmail.com>
2026-09-15 14:01:59 +0500

    Remove mouse interaction patch causing click lock

--- ui_patches.py
+++ ui_patches.py
@@ -6,7 +6,7 @@ def inject_persistent_download_ui(response):
     try:
         if request.path == "/" and response.content_type and response.content_type.startswith("text/html"):
             body = response.get_data(as_text=True)
-            markers = ('<script src="/static/persistent_download_ui.js"></script>','<script src="/static/special_preview_ui.js"></script>','<script src="/static/text_selection_patch.js"></script>','<script src="/static/native_input_interaction_patch.js"></script>','<script src="/static/progress_size_stability_patch.js"></script>','<script src="/static/folder_action_position_patch.js"></script>','<script src="/static/final_ui_reliability_patch.js"></script>')
+            markers = ('<script src="/static/persistent_download_ui.js"></script>','<script src="/static/special_preview_ui.js"></script>','<script src="/static/text_selection_patch.js"></script>','<script src="/static/progress_size_stability_patch.js"></script>','<script src="/static/folder_action_position_patch.js"></script>','<script src="/static/final_ui_reliability_patch.js"></script>')
             if "</body>" in body:
                 for marker in markers:
                     if marker not in body:
